2d8dec37b824c8fc987bee5b1d8caac32663839b
Anthony M <anthonyme1211@gmail.com>
2021-03-12 17:26:23 +0700

    First commit

---
+++ Dataset/Dataset.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+
+namespace MachineLearning.Dataset
+{
+    class ImageLabelSet
+    {
+        public int Count { get; private set; }
+        public int ColumnCount { get; private set; }
+        public int RowCount { get; private set; }
+
+        public byte[] Labels { get; private set; }
+        public byte[] Data { get; private set; }
+
+        public static ImageLabelSet LoadDataset(string imageFile, string labelFile)
+        {
+            ImageLabelSet dataset = new ImageLabelSet();
+
+            using (FileStream fs = File.OpenRead(imageFile))
+            {
+                byte[] infoBuffer = new byte[4];
+
+                fs.Read(infoBuffer, 0, 4);
+                fs.Read(infoBuffer, 0, 4);
+                if (BitConverter.IsLittleEndian)
+                {
+                    Array.Reverse(infoBuffer);
+                }
+                Int32 dataCount = BitConverter.ToInt32(infoBuffer);
+                Console.WriteLine(string.Format("Images in dataset: {0}", dataCount));
+                fs.Read(infoBuffer, 0, 4);
+                if (BitConverter.IsLittleEndian)
+                {
+                    Array.Reverse(infoBuffer);
+                }
+                Int32 rowCount = BitConverter.ToInt32(infoBuffer);
+                Console.WriteLine(string.Format("Number of rows: {0}", rowCount));
+                fs.Read(infoBuffer, 0, 4);
+                if (BitConverter.IsLittleEndian)
+                {
+                    Array.Reverse(infoBuffer);
+                }
+                Int32 colCount = BitConverter.ToInt32(infoBuffer);
+                Console.WriteLine(string.Format("Number of cols: {0}", colCount));
+
+                dataset.Count = dataCount;
+                dataset.RowCount = rowCount;
+                dataset.ColumnCount = colCount;
+
+                byte[] dataBuffer = new byte[dataCount * rowCount * colCount];
+                fs.Read(dataBuffer, 0, dataBuffer.Length);
+                dataset.Data = dataBuffer;
+            }
+
+            using (FileStream fs = File.OpenRead(labelFile))
+            {
+                byte[] infoBuffer = new byte[4];
+
+                fs.Read(infoBuffer, 0, 4);
+                fs.Read(infoBuffer, 0, 4);
+                if (BitConverter.IsLittleEndian)
+                {
+                    Array.Reverse(infoBuffer);
+                }
+                Int32 dataCount = BitConverter.ToInt32(infoBuffer);
+
+                byte[] labelBuffer = new byte[dataCount];
+                fs.Read(labelBuffer, 0, dataCount);
+                dataset.Labels = labelBuffer;
+            }
+
+            return dataset;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using MachineLearning.Dataset;
+using System;
+using System.IO;
+
+namespace MachineLearning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ImageLabelSet dataset = ImageLabelSet.LoadDataset("dataset/train-images.idx3-ubyte", "dataset/train-labels.idx1-ubyte");
+
+            Console.WriteLine(dataset.Count);
+            Console.WriteLine(dataset.RowCount);
+            Console.WriteLine(dataset.ColumnCount);
+
+            for (int i = 0; i < dataset.Count; i++)
+            {
+                for (int y = 0; y < dataset.RowCount; y++)
+                {
+                    for (int x = 0; x < dataset.ColumnCount; x++)
+                    {
+                        if (dataset.Data[i * dataset.ColumnCount * dataset.RowCount + y * dataset.ColumnCount + x] > 127)
+                        {
+                            Console.Write("#");
+                        }
+                        else
+                        {
+                            Console.Write(" ");
+                        }
+                    }
+                    Console.Write("\n");
+                }
+                Console.WriteLine("Number is: " + dataset.Labels[i]);
+
+                if (i > 5) break;
+            }            
+
+            Console.WriteLine(dataset.Labels.Length);
+        }
+    }
+}
